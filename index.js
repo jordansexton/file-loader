c33edc629ac406ab97f3f8ee88e3570b41cd1fae
Jordan Sexton <jordan@jordansexton.com>
2015-07-02 19:21:23 -0500

    Added emitFile option

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ var loaderUtils = require("loader-utils");
 
 module.exports = function(content) {
 	this.cacheable && this.cacheable();
-	if(!this.emitFile) throw new Error("emitFile is required from module system");
 	var query = loaderUtils.parseQuery(this.query);
+	if(!this.emitFile && query.emitFile !== false) throw new Error("emitFile is required from module system");
 	var url = loaderUtils.interpolateName(this, query.name || "[hash].[ext]", {
 		context: query.context || this.options.context,
 		content: content,
 		regExp: query.regExp
 	});
-	this.emitFile(url, content);
+	if (query.emitFile !== false) {
+		this.emitFile(url, content);
+	}
 	return "module.exports = __webpack_public_path__ + " + JSON.stringify(url) + ";";
 }
 module.exports.raw = true;
